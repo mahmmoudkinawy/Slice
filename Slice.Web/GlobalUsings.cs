7109119d41a3b5819bb33a5915941babd15024c5
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2022-03-06 13:20:05 +0200

    Added Get all UI and it's logic

--- Slice.Web/GlobalUsings.cs
+++ Slice.Web/GlobalUsings.cs
@@ -2,3 +2,5 @@
 global using Microsoft.EntityFrameworkCore;
 global using Slice.Web.Entities;
 global using Slice.Web.Data;
+global using Microsoft.AspNetCore.Mvc.RazorPages;
+global using Microsoft.AspNetCore.Mvc;
---
+++ Slice.Web/Pages/Categories/Create.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Slice.Web.Pages.Categories
+{
+    public class CreateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
