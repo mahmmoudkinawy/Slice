73751db69a24470c12cb47ccaea69b4b2c494e27
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2022-03-06 16:03:19 +0200

    Added delete UI and it's logic

---
+++ Slice.Web/Pages/Categories/Delete.cshtml.cs
@@ -0,0 +1,26 @@
+namespace Slice.Web.Pages.Categories;
+public class DeleteModel : PageModel
+{
+    private readonly SliceDbContext _context;
+
+    [BindProperty]
+    public Category Category { get; set; }
+
+    public DeleteModel(SliceDbContext context) => _context = context;
+
+    public async Task OnGetAsync(int id)
+        => Category = await _context.Categories.FindAsync(id);
+
+    public async Task<IActionResult> OnPostAsync()
+    {
+        var categoryToDelete = await _context.Categories.FindAsync(Category.Id);
+        if (categoryToDelete != null)
+        {
+            _context.Categories.Remove(categoryToDelete);
+            await _context.SaveChangesAsync();
+            return RedirectToPage("Index");
+        }
+        return Page();
+    }
+
+}
