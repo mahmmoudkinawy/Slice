ca58b8b74b770cdf8753471092565ee50c1b1e85
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2022-03-13 13:00:06 +0200

    Added validation for uploading and image

--- Slice.Web/Controllers/ProductsController.cs
+++ Slice.Web/Controllers/ProductsController.cs
@@ -12,4 +12,13 @@ public class ProductsController : Controller
     [HttpGet]
     public async Task<IActionResult> GetProducts()
         => Json(new { data = await _productRepository.GetAllAsync("Category,FoodType") });
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteProduct(int id)
+    {
+        var productFromDb = await _productRepository.GetFirstOrDefaultAsync(p => p.Id == id);
+        await _productRepository.Remove(productFromDb);
+        return Json(new { success = true, message = "Deleted Successfully" });
+    }
+
 }
--- Slice.Web/wwwroot/js/products.js
+++ Slice.Web/wwwroot/js/products.js
@@ -1,22 +1,23 @@
-﻿$(document).ready(function () {
-    $('#loadData').DataTable({
+﻿var dataTable;
+$(document).ready(function () {
+    dataTable = $('#loadData').DataTable({
         "ajax": {
-            "url": "/api/products",
+            "url": "/api/Products",
             "type": "GET",
-            "dataType": "json"
+            "datatype": "json"
         },
         "columns": [
             { "data": "name", "width": "25%" },
-            { "data": "price", "width": "25%" },
-            { "data": "foodType.name", "width": "25%" },
-            { "data": "category.name", "width": "25%" },
+            { "data": "price", "width": "15%" },
+            { "data": "category.name", "width": "15%" },
+            { "data": "foodType.name", "width": "15%" },
             {
                 "data": "id",
                 "render": function (data) {
                     return `<div class="w-75 btn-group" >
-                            <a href="/Admin/products/upsert?id=${data}"  class="btn btn-success text-white mx-2">
+                            <a href="/Admin/Productss/upsert?id=${data}"  class="btn btn-success text-white mx-2">
                             <i class="bi bi-pencil-square"></i>  </a>
-                            <a   class="btn btn-danger text-white mx-2">
+                            <a onClick="Delete('/api/Products/'+${data})"  class="btn btn-danger text-white mx-2">
                              <i class="bi bi-trash-fill"></i>  </a>
                             </div>`
                 },
@@ -27,3 +28,35 @@
         "width": "100%"
     });
 });
+
+
+function Delete(url) {
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            $.ajax({
+                url: url,
+                type: 'DELETE',
+                success: function (data) {
+                    if (data.success) {
+                        dataTable.ajax.reload();
+                        //success notification
+                        toastr.success(data.message);
+                    }
+                    else {
+                        //failsure notification
+                        toastr.error(data.message);
+                    }
+                }
+            })
+        }
+    })
+
+}
