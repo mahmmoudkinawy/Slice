e950dd05a498ca7fb61aeb041e7b30c7ece8dd6e
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2022-03-18 17:07:26 +0200

    Added cart total calculation

--- Slice.Web/Pages/Customer/ShoppingCart/Index.cshtml.cs
+++ Slice.Web/Pages/Customer/ShoppingCart/Index.cshtml.cs
@@ -4,18 +4,20 @@ namespace Slice.Web.Pages.Customer.ShoppingCart;
 public class IndexModel : PageModel
 {
     private readonly ICartRepository _cartRepository;
+    public double CartTotal { get; set; }
     public IReadOnlyList<Cart> CartList { get; set; }
 
     public IndexModel(ICartRepository cartRepository)
         => _cartRepository = cartRepository;
 
     public async Task OnGet()
-    //=> CartList = await _cartRepository.GetAllAsync(
-    //    filter: u => u.AppUserId == User.GetUserId());
     {
         CartList = await _cartRepository.GetAllAsync(
            filter: u => u.AppUserId == User.GetUserId(),
            includeProperties: "Product,Product.Category,Product.FoodType");
+
+        foreach (var item in CartList)
+            CartTotal += (item.Product.Price * item.Count);
     }
 
 }
