57c3d24f712a25e947734c5e33cd1a8cc44baf76
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2022-04-01 15:50:16 +0200

    Added send email for confirmation using send grid service

--- Slice.Utilities/EmailSender.cs
+++ Slice.Utilities/EmailSender.cs
@@ -1,6 +1,24 @@
 ﻿namespace Slice.Utilities;
 public class EmailSender : IEmailSender
 {
+    private readonly IConfiguration _config;
+
+    public string SendGridKey { get; set; }
+
+    public EmailSender(IConfiguration config) => _config = config;
+
     public Task SendEmailAsync(string email, string subject, string htmlMessage)
-        => Task.CompletedTask;
+    {
+        //There is a fucking bug here!! I do not know what is it!!!
+
+
+        SendGridKey = _config.GetValue<string>("SendGrid:SecretKey");
+
+        var client = new SendGridClient(SendGridKey);
+        var from = new EmailAddress("mahmmoudkinawy@gmail.com", "Slice Pizza");
+        var to = new EmailAddress(email);
+        var message = MailHelper.CreateSingleEmail(from, to, subject, "", htmlMessage);
+
+        return client.SendEmailAsync(message);
+    }
 }
--- Slice.Utilities/GlobalUsings.cs
+++ Slice.Utilities/GlobalUsings.cs
@@ -1,6 +1,9 @@
-﻿global using Microsoft.AspNetCore.Identity.UI.Services;
+﻿global using CloudinaryDotNet;
 global using CloudinaryDotNet.Actions;
 global using Microsoft.AspNetCore.Http;
-global using CloudinaryDotNet;
+global using Microsoft.AspNetCore.Identity.UI.Services;
+global using Microsoft.Extensions.Configuration;
 global using Microsoft.Extensions.Options;
+global using SendGrid;
+global using SendGrid.Helpers.Mail;
 global using Slice.Utilities.Helpers;
